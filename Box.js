9a389000a5d5f3caaddc5440be83b983cf75046c
Tanav-Mittal <68111690+Tanav-Mittal@users.noreply.github.com>
2020-08-23 08:57:23 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -5,8 +5,7 @@ class Box
         var options = 
         {
             restitution:0.8,
-            friction:1,
-            density:1
+            friction:0,
         }
 
         this.body = Bodies.rectangle(x,y,width,height,options)
